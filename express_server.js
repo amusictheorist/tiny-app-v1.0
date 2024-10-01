cfcccc8e0874ede973bc7badee00cea459c3285c
amusictheorist <a_millaresthomson@outlook.com>
2024-09-30 20:02:05 -0400

    Added urls_show

--- express_server.js
+++ express_server.js
@@ -17,9 +17,18 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urls.json", (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = { 
+    id,
+    longURL: urlDatabase[id]
+  }
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
