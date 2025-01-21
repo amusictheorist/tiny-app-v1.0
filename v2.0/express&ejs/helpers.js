da646696c083f6891f8b51fe3d459f9ed7362224
amusictheorist <a_millaresthomson@outlook.com>
2025-01-21 12:18:09 -0500

    Update top level .gitignore

--- v2.0/express&ejs/helpers.js
+++
@@ -1,8 +0,0 @@
-// Helper functions to run TinyApp and express_server.js
-
-const generateRandomString = function() {
-  let result = Math.random().toString(36).slice(2);
-  return result;
-};
-
-module.exports = { generateRandomString };
